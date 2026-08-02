bbb0b296151b91563922b7295732744062046457
Pedro Verde <pedroverde@gmail.com>
2026-08-02 17:07:21 +0100

    Add player animations and config player animator. Add platerAnimator script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ public class Player : MonoBehaviour {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float rotateSpeed = 10f;
 
+    private bool isWalking = false;
+
     private void Update() {
         Vector2 inputVector = new Vector2(0, 0);
         if (Input.GetKey(KeyCode.W)) {
@@ -27,7 +29,14 @@ public class Player : MonoBehaviour {
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        isWalking = moveDir != Vector3.zero;
+
         transform.forward = Vector3.
             Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);    
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
