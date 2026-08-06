83053c2c5f6eec37fb2ebd13ac7e1211c109f42f
Pedro Verde <pedroverde@gmail.com>
2026-08-06 16:38:25 +0100

    Add event logic for interaction trigger

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,10 +11,37 @@ public class Player : MonoBehaviour {
     private bool isWalking = false;
     private Vector3 lastInteractDirection;
 
+    private void Start()
+    {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, EventArgs e)
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDirection = moveDir;
+        }
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDirection, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                //Has clearCounter
+                clearCounter.Interact();
+            }
+        }
+    }
+
     private void Update()
     {
         HandleMovement();
-        HandleInteractions();
+        //HandleInteractions();
     }
 
     private void HandleInteractions()
