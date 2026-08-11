8ee3b31e1f9079aade4059d8ff7fb16e916dfdd8
Pedro Verde <pedroverde@gmail.com>
2026-08-11 11:03:26 +0100

    Refactor code and implemente player pick and drop kitchen objects. Add several new kitchen objects

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -6,8 +6,29 @@ public class ClearCounter : BaseCounter
     [SerializeField] private KitchenObjectSO kitchenObjectSO;    
 
     public override void Interact(Player player)
-    {       
-
+    {
+        if (!HasKitchenObject()) 
+        {
+            //Counter has no kitchen object
+            if (player.HasKitchenObject())
+            {
+                //Player is carrying kitchen object
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+        }
+        else
+        {
+            //counter has kitchen object
+            if (player.HasKitchenObject()) 
+            {
+                //player is carrying kitchen object
+            }
+            else
+            {
+                //player is not carrying kitchenObject
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
     
 }
