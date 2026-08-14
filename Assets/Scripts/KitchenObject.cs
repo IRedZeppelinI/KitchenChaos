0324cb675f5ccaf86fa52c1511956ae77ce5a0e3
Pedro Verde <pedroverde@gmail.com>
2026-08-14 16:29:30 +0100

    Add plate ingridient relation. Implement add and validate ingridients to plate

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -26,8 +26,7 @@ public class KitchenObject : MonoBehaviour
     }
     public IKitchenObjectParent GetKitchenObjectParent() => kitchenObjectParent;
 
-        
-
+     
     public void DestroySelf()
     {
         kitchenObjectParent.ClearKitchenObject() ;
@@ -35,7 +34,19 @@ public class KitchenObject : MonoBehaviour
         Destroy(gameObject);
     }
 
-
+    public bool TryGetPlate(out PlateKitchenObject plateKitchenObject)
+    {
+        if (this is PlateKitchenObject)
+        {
+            plateKitchenObject = this as PlateKitchenObject;
+            return true;
+        }
+        else
+        {
+            plateKitchenObject = null;
+            return false;
+        }
+    }
 
     public static KitchenObject SpawnKitchenObject(KitchenObjectSO kitchenObjectSO, IKitchenObjectParent kitchenObjectParent)
     {
